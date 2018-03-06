b899705697ac0cc1dfa50ea6937a7190a8ae41d6
Tyler Buter <tbutler@bu.edu>
2018-03-06 07:20:49 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,9 +10,6 @@ int main()
     ThunderborgDriver thunderborgDriver;
     thunderborgDriver.initialize();
     MotorController motorController(thunderborgDriver);
-    //motorController.performMove(FORWARD, FORWARD, 1000);
-    //motorController.performSpin(-355);
-    //motorController.driveBackwords(100);
 
     char letter;
     while (letter != 'q')
@@ -35,19 +32,6 @@ int main()
             motorController.performSpin(10);
         }
     }
-    /**
-    thunderborgDriver.setLedColor(0, 50, 0);
-    //thunderborgDriver.test();
-    thunderborgDriver.checkForThunderborg();
-    float voltage = thunderborgDriver.getBatteryReading();
-    std::cout << "Current battery voltage: " << voltage << std::endl;
-    //thunderborgDriver.setMotor1(-0.95);
-    //thunderborgDriver.setMotor2(-0.95);
-    thunderborgDriver.setMotorPower(1, 2, 243);
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    thunderborgDriver.motorsOff();
-    //thunderborgDriver.setMotor2(0);
-    //printf("hello2\n");
-    **/
+    
     return 0;
 }
